ef338cd88ca2d44cc2adf420bf7ba566c21be915
electimon <electimon@gmail.com>
2022-07-25 10:22:52 +0000

    media: Remove guarding
    Signed-off-by: electimon <electimon@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,11 +1,8 @@
 ifneq ($(filter 5.4, $(KERNEL_VERSION)),)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
-    include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-    include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-    include $(QCOM_MEDIA_ROOT)/libplatformconfig/Android.mk
-endif
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libplatformconfig/Android.mk
 
 endif
